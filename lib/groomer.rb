baebceb33ebc1e26b246d9d501d78eebb8e64097
Anhnhi Tran <anhtran811@gmail.com>
2022-10-26 18:27:36 -0600

    Iteration 3 incomplete for count pet type

--- lib/groomer.rb
+++ lib/groomer.rb
@@ -1,28 +1,42 @@
 class Groomer
-  attr_reader :name,
-              :customers
+attr_reader :name, :customers
 
   def initialize(name)
     @name = name
     @customers = []
   end
 
-  def add_customer(customer)
+  def add_customers(customer)
     @customers << customer
   end
 
-  def customers_with_oustanding_balances
-    @customers.find_all do |customer|
-      customer.outstanding_balance != 0
+  def customer_outstanding_balance
+    customer_with_balance = []
+    @customers.each do |customer|
+      if customer.outstanding_balance != 0
+        customer_with_balance << customer
+      end
     end
   end
 
-  def number_of_pets(type)
-    pets = @customers.map do |customer|
-      customer.pets
-    end.flatten
-    pets.count do |pet|
-      pet.type == type
+  def count_pet_by_type
+  customer_pets = []
+    @customers.each do |customer|
+      customer_pets << customer.pets
+    end
+    customer_pets
+    pet_by_type = []
+    customer_pets.each do |pet_type|
+      require 'pry'; binding.pry
     end
   end
 end
+
+# nested arrays --> flatten
+# collected then counted
+# count enumerable
+
+
+# * A Groomer can count the number of pets of a given type. 
+#For example the groomer can count the number of cats, the number of dogs, etc. 
+# This should work for any pet type.
