1fbff17e7b75723307e4108886715a5f7b119167
Anhnhi Tran <anhtran811@gmail.com>
2022-10-26 18:27:02 -0600

    Completed iteration 2 tests in customer spec

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -2,41 +2,53 @@ require './lib/customer'
 require './lib/pet'
 
 RSpec.describe Customer do
-  before(:each) do
-    @joel = Customer.new("Joel", 2)
+  it 'exists' do
+    joel = Customer.new("Joel", 2)
+
+    expect(joel).to be_a(Customer)
   end
 
-  it "exists" do
-    expect(@joel).to be_instance_of(Customer)
+  it 'has a name' do
+    joel = Customer.new("Joel", 2)
+
+    expect(joel.name).to eq("Joel")
   end
 
-  it "has attributes" do
-    expect(@joel.name).to eq("Joel")
-    expect(@joel.id).to eq(2)
+  it 'has an id' do
+    joel = Customer.new("Joel", 2)
+
+    expect(joel.id).to eq(2)
   end
 
-  it "starts with no pets" do
-    expect(@joel.pets).to eq([])
+  it 'starts with no pets' do
+    joel = Customer.new("Joel", 2)
+
+    expect(joel.pets).to eq([])
   end
 
-  it "can adopt new pets" do
-    samson = Pet.new({name: "Samson", type: :dog})
-    lucy = Pet.new({name: "Lucy", type: :cat})
+  it 'can adopt pets' do
+    joel = Customer.new("Joel", 2) 
+    samson = Pet.new({name: "Samson", type: :dog, age: 3})
+    lucy = Pet.new({name: "Lucy", type: :cat, age: 12})
 
-    @joel.adopt(samson)
-    @joel.adopt(lucy)
-    expect(@joel.pets).to eq([samson, lucy])
+    joel.adopt(samson)
+    joel.adopt(lucy)
+
+    expect(joel.pets).to eq([samson, lucy])
   end
 
-  it "has starting outstanding balance at 0" do
-    expect(@joel.outstanding_balance).to eq(0)
+  it 'can has a starting balance of 0' do
+    joel = Customer.new("Joel", 2)
+
+    expect(joel.outstanding_balance).to eq(0)
   end
 
-  it "can charge to increase oustanding balance" do
-    @joel.charge(15)
-    @joel.charge(7)
+  it 'can charge to the balance' do
+    joel = Customer.new("Joel", 2) 
 
-    expect(@joel.outstanding_balance).to eq(22)
+    joel.charge(15)
+    joel.charge(7)
+
+    expect(joel.outstanding_balance).to eq(22)
   end
-
 end
